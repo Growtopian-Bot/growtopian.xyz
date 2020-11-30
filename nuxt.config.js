5a1408e0c40b38bc2ec98a125f9c52f07d587769
AykutSarac <aykutsarac0@gmail.com>
2020-11-30 17:30:39 +0300

    defer js

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,6 +40,7 @@ export default {
         integrity:
           "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo",
         crossorigin: "anonymous",
+        defer: true
       },
       {
         src:
@@ -47,6 +48,7 @@ export default {
         integrity:
           "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1",
         crossorigin: "anonymous",
+        defer: true
       },
       {
         src:
@@ -54,6 +56,7 @@ export default {
         integrity:
           "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM",
         crossorigin: "anonymous",
+        defer: true
       },
     ]
   },
