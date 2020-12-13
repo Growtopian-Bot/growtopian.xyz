e1d0081f8279171374b42d209673291a65305511
AykutSarac <aykutsarac0@gmail.com>
2020-12-13 17:51:49 +0300

    premium page

--- nuxt.config.js
+++ nuxt.config.js
@@ -93,7 +93,7 @@ export default {
       routes: [
         '/',
         '/commands',
-        '/donate',
+        '/premium',
         '/continuity-of-service'
       ]
     }]
--- pages/premium.vue
+++ pages/premium.vue
@@ -1,3 +1,7 @@
+<template>
+    <div>Redirecting you now...</div>
+</template>
+
 <script>
 window.location.href = 'https://patreon.com/join/phemus';
 </script>
