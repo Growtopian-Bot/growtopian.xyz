f4b66bca299bc262abf033f0717a8dd8c67dc9d7
AykutSarac <aykutsarac0@gmail.com>
2020-12-13 12:40:54 +0300

    adsense edit

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@
         <Content />
         <Features />
         <Invite />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+            style="display:block"
+            data-ad-format="fluid"
+            data-ad-layout-key="-h4-k+2y-66+5y"
+            data-ad-client="ca-pub-8568718403889933"
+            data-ad-slot="9257683973"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
     </div>
 </template>
 
