516f5c85bceec1887265ef45b6cd012636b4b896
AykutSarac <aykutsarac0@gmail.com>
2020-12-17 14:52:45 +0300

    new page

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,83 @@
 <template>
     <div class="footer">
-        <div class="text">
-        © 2020 Growtopian - The Discord Bot. All sprites and related graphics are owned by Growtopia and Ubisoft Entertainment® - Growtopian is in no way affiliated with Ubisoft Entertainment® or Growtopia.
+        <div class="flex">
+            <div class="links">
+                <h5>Other Nav</h5>
+                <ul>
+                    <li><RouterLink to="/rules">Rules</RouterLink></li>
+                    <li><RouterLink to="/continuity-of-service">Continuity of Service</RouterLink></li>
+                    <li><RouterLink to="/commands">Commands</RouterLink></li>
+                </ul>
+            </div>
+            <div class="text">
+             © 2020 Growtopian - The Discord Bot. All sprites and related graphics are owned by Growtopia and Ubisoft Entertainment® - Growtopian is in no way affiliated with Ubisoft Entertainment® or Growtopia.
+            </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.footer {
+.flex {
+    display: flex;
     color: oldlace;
     bottom: 0;
-    text-align: center;
+    justify-content: center;
+
     margin-top: 10%;
+
     background-color: darkslategray;
-    min-height: 5em;
+    min-height: 10em;
     padding: 1em;
-    height: fit-content;
+
+    div {
+        margin: auto;
+    }
+
+    h5 {
+            font-weight: bolder;
+        }
+
+    .links ul {
+        list-style: none;
+        padding: 0;
+
+        li a {
+            text-decoration: none;
+            color: oldlace;
+
+            &:hover {
+                color: rgb(130, 207, 238);
+            }
+        }
+    }
+
+    .text {
+        width: 35%;
+    }
 
 }
+
+@media only screen and (max-width: 768px) {
+    
+        .flex {
+            text-align: center;
+            display: block;
+
+            .links {
+                text-align: left;
+            }
+
+            .text {
+                border-top: 1px rgb(128, 128, 128) solid;
+                padding-top: 5px;
+                font-size: 12px;
+                width: 100%;
+                margin: 0;
+            }
+        }
+
+        
+
+        
+    }
 </style>
---
+++ pages/rules.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="wrap">
+        
+        <div class="rules">
+            <hr>
+            <h2>Growtopian Rules</h2>
+            <h6>By using Growtopian, you agree to these rules. If you break any rules, we reserve the right to ban you from Growtopian.</h6>
+            <div class="rule" v-for="(rule, key) in rules" :key="key">
+                <h4>{{ rule.title }}</h4>
+                {{ rule.content }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+const rules = [
+    {title: "Userbots, Selfbotting", content: "Userbotting, macros, scripts and anything else used to automate running commands is strictly forbidden. On top of this, massive amounts of spam is not allowed and will be punished with the same severity."},
+    {title: "Sharing Exploits", content: "It's prohibited to share exploits/bugs with other users. Please report all bugs to staff of Growtopian's Support Server so we can fix them as soon as possible."},
+    {title: "Alt Farming", content: "It is prohibited to use alt accounts to farm or store currencies. This involves supplying real people who don't use the currency system with gems/world locks."},
+    {title: "Scamming", content: "It is prohibited to use bot currency (or any of the bot's features) to scam, trade or sell something. Giving gems/world locks, for instance, in exchange for individuals advertising your server."},
+    {title: "Discord ToS", content: "The Discord TOS and Discord Community Guidelines also are enforcable through our bot."},
+    {title: "The Growtopian Code", content: "If we find out you're violating the Growtopian Code, for example your server is being used for blackmarketing we'll remove the bot from your server instantly and report it to Discord immediately."}
+]
+
+export default {
+    data() {
+        return {
+            rules
+        }
+    },
+    mounted() {
+    document.body.style.backgroundColor = "#67a8f0";
+  }
+}
+</script>
+
+body {
+
+}
+
+<style lang="scss" scoped>
+
+.rules {
+    text-align: center;
+
+    h2 {
+        margin-top: 2em;
+        color: #ffd900;
+    }
+
+    h6 {
+        color: #ffecec;
+        margin-bottom: 6em;
+    }
+}
+
+.rule {
+    color: white;
+    background-color: #2d6187;;
+    margin: 5% 25%;
+    line-height: 1.6em;
+    border-radius: 10px;
+    padding: 1em;
+
+    h4 {
+        color: rgb(253, 192, 77);
+    }   
+}
+
+@media only screen and (max-width: 768px) {
+    .rule {
+            width: 90%;
+            margin: 15px;
+        }
+}
+</style>
