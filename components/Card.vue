00c52148aab9af74ab5c70bddbf9e1c00b6ce652
AykutSarac <aykutsarac0@gmail.com>
2021-07-23 19:08:36 +0300

    Fixed card bg color

--- components/Card.vue
+++ components/Card.vue
@@ -8,26 +8,29 @@
 
 <script>
 export default {
-  name: "Card"
-}
+  name: "Card",
+};
 </script>
 
 <style lang="scss" scoped>
-  .card {
-    font-weight: 300;
-    font-size: 20px;
-    max-width: 1077px;
-    margin: 1em auto;
+.card {
+  font-weight: 300;
+  font-size: 20px;
+  max-width: 1077px;
+  margin: 1em auto;
+  border-radius: 10px;
+  padding: 0.5em;
+
+  &.hero__motto {
     background: rgba(255, 255, 255, 0.836);
-    border-radius: 10px;
-    padding: 0.5em;
-
-    .card__title {
-      font-size: 24px;
-    }
-
-    .card__contents {
-      font-size: 14px;
-    }
   }
+
+  .card__title {
+    font-size: 24px;
+  }
+
+  .card__contents {
+    font-size: 14px;
+  }
+}
 </style>
